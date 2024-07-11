9a6b9982245ed64cd35cdfdf6a71ed449cd10c28
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-07-11 14:22:36 +0700

    chore: rm deprecated comments

--- prettier.config.js
+++ prettier.config.js
@@ -19,33 +19,4 @@ export default {
   ],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderTypeScriptVersion: "5.0.0",
-  // importOrder: [
-  //   // Side effect imports.
-  //   "^\\u0000",
-  //   "",
-  //   "<BUILT_IN_MODULES>",
-  //   "",
-  //   "<THIRD_PARTY_MODULES>",
-  //   "",
-  //   // Packages.
-  //   // Things that start with a letter (or digit or underscore), or
-  //   // `@` followed by a letter.
-  //   "^@?\\w",
-  //   "",
-  //   "^[./]",
-  //   "",
-  //   "<TYPES>",
-  //   "",
-  //   // // Root imports
-  //   // "^(src)(/.*|$)",
-  //   // "^(tests)(/.*|$)",
-  //   // // Parent imports. Put `..` last.
-  //   // "^\\.\\.(?!/?$)",
-  //   // "^\\.\\./?$",
-  //   // // Other relative imports. Put same-folder imports and `.` last.
-  //   // "^\\./(?=.*/)(?!/?$)",
-  //   // "^\\.(?!/?$)",
-  //   // "^\\./?$",
-  // ],
-  // importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
 };

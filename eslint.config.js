9897646f87421f192a31dd050d4a045dbe9bfc0b
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-07-11 14:28:47 +0700

    fix: remove deprecated code

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-// import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [eslintPluginPrettierRecommended];
